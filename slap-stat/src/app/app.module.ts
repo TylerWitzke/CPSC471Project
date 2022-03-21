d824babf4908f611c31f41d8add4606d576c4b57
TylerWitzke <twitzke6@outlook.com>
2022-03-20 21:38:09 -0600

    Created the login, register, and home buttons. Also added a stock image for the time being

--- slap-stat/src/app/app.module.ts
+++ slap-stat/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponentComponent } from './components/registration-component/registration-component.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyMaterialModule } from  './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
