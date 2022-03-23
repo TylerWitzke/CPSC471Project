a813d8cbd8c740552f1c6abf0652b19dd80d8894
Dillon Matthews <dillon.matthews2001@gmail.com>
2022-03-22 18:08:06 -0600

    Added more to login

--- slap-stat/src/app/components/login-component/login.component.ts
+++ slap-stat/src/app/components/login-component/login.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent implements OnInit {
+  mode: string = '';
+  email: string = '';
+  password: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  togglePlayerMode(){
+    this.mode = 'Player';
+  }
+  toggleCoachMode(){
+    this.mode = 'Coach';
+  }
+  login(){
+    console.log('login code');
   }
 
 }
