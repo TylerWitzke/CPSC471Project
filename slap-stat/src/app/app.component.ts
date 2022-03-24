a01526468e427c1b82dc47ad674656baa3a86cf2
TylerWitzke <twitzke6@outlook.com>
2022-03-24 10:21:39 -0600

    Added more to the user register, still need to make selecting from radio buttons more efficient

--- slap-stat/src/app/app.component.ts
+++ slap-stat/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+export interface Player {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  height: number;
+  weight: number;
+  handness: string;
+  number: number;
+  position: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- slap-stat/src/app/components/register/register.component.ts
+++ slap-stat/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from 'src/app/app.component';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   player: boolean = false;
+  editPlayer: Player = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  password: '',
+  height: 0,
+  weight: 0,
+  handness: '',
+  number: 0,
+  position: ''
+  }
+
   constructor() { }
 
   
@@ -16,9 +29,19 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
   //This toggles the boolean for player
-  togglePlayer(){
-    this.player = !this.player;
+  togglePlayerTrue(){
+    this.player = true;
   }
+
+    //This toggles the boolean for player
+    togglePlayerFalse(){
+      this.player = false;
+    }
+
+    //This is what will send the player to the destination
+    registerPerson(){
+      console.log(this.editPlayer.first_name);
+    }
 
     //Return the player function
     getPlayer() :boolean
