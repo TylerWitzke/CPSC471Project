7fa81dc97604dcb67aea66a512bcdf821a5c7bc4
Dillon Matthews <dillon.matthews2001@gmail.com>
2022-04-05 17:26:29 -0600

    save my database

--- slap-stat/src/app/components/player-home/player-home.component.ts
+++ slap-stat/src/app/components/player-home/player-home.component.ts
@@ -25,11 +25,18 @@ export class PlayerHomeComponent implements OnInit {
   }
   team: Team = {
     team_id: 1,
-    team_record: '24-9-3',
+    team_record: '',
     name: '',
     division: '',
     league: ''
 
+  }
+  team_stats = {
+    wins: 0,
+    losses: 0,
+    shots: 0,
+    shotsAgainst: 0,
+    pims: 0
   }
   player_stats = {
     goals: 55,
@@ -62,6 +69,13 @@ export class PlayerHomeComponent implements OnInit {
     this.team.team_id = id;
     this.teamService.getTeam(id).subscribe(response =>{this.teams = response;
       if(response){
+        this.getTeamStats(id);
+      }});
+  }
+  getTeamStats(id: number){
+    
+    this.teamStatService.getTeamStats(id).subscribe(response =>{this.teamStats = response;
+      if(response){
         this.populateTeam();
       }});
   }
@@ -69,6 +83,18 @@ export class PlayerHomeComponent implements OnInit {
     this.team.name = this.teams[0].Name;
     this.team.league = this.teams[0].League;
     this.team.division = this.teams[0].Division;
+    let w = this.teamStats[0].Wins;
+    let l = this.teamStats[0].Losses;
+    let s = '';
+    s+=w;
+    s += ' - ';
+    s += l;
+    this.team.team_record = s;
+    this.team_stats.wins = this.teamStats[0].Wins;
+    this.team_stats.losses = this.teamStats[0].Losses;
+    this.team_stats.pims = this.teamStats[0].PIMS;
+    this.team_stats.shotsAgainst = this.teamStats[0].shotsAgainst;
+    console.log(this.team_stats);
   }
 
 
