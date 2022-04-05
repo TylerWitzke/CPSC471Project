fb7ed5765016aa822dbe9b3c88d6cd8bd06eadd8
TylerWitzke <twitzke6@outlook.com>
2022-04-05 14:20:36 -0600

    RIP database

--- slap-stat/src/app/services/player-stats.service.ts
+++ slap-stat/src/app/services/player-stats.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PlayerStatsService {
+  readonly APIUrl = "http://127.0.0.1:6969/";
+  constructor(private http:HttpClient) { }
+  getPersonnal(email:any):Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl +'personnal/'+email);
+  }
 
-  constructor() { }
+  addPersonnal(val:any){
+    return this.http.post(this.APIUrl + 'personnal/',val);
+  }
+
+  putPersonnal(val:any){
+    return this.http.put(this.APIUrl + 'personnal/',val);
+  }
+
+  deletePersonnal(val:any){
+    return this.http.delete(this.APIUrl + 'personnal/'+val);
+  }
 }
